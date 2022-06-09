4544e2012d3ec4b7d86dfd6b019b489bdf6d5694
camgany <you@example.com>
2022-06-09 06:26:23 -0400

    HOMEWORK STATE 3.0

--- src/main/java/state/practice/ConcretePrendido.java
+++ src/main/java/state/practice/ConcretePrendido.java
@@ -10,9 +10,16 @@ public class ConcretePrendido implements IState {
     public void handle(Computadora context) {
         int a;
         a = new Random().nextInt(20);
+        String[] b = new String[a];
         System.out.println("-------------------------PRENDIDO--------------------------");
-        System.out.println(a);
-        context.setProgramaAbierto(new String[a]);
+        //System.out.println(a);
+        for(int i = 0; i < a; i++){
+            b [i]= "Google Chrome";
+        }
+        context.setProgramaAbierto(b);
+        for(int i = 0; i< a; i++){
+            System.out.println("Recurso: "+b[i]);
+        }
         System.out.println("Cantidad de recursos abiertos: "+context.getProgramaAbierto().length);
         context.getConsumoMemoria().setConsumo(a*5);
         System.out.print("Consumo de Memoria RAM: " +context.getConsumoMemoria().getConsumo());
